25d67c0b05a4a7acd309bdc86b745d45cc391287
RobertoJavaDev <robertojavadev@gmail.com>
2023-06-08 09:06:23 +0200

    initial commit

---
+++ src/main/java/pl/robertojavadev/githubrepositoriesservice/GithubRepositoriesServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.robertojavadev.githubrepositoriesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoriesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepositoriesServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/robertojavadev/githubrepositoriesservice/GithubRepositoriesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.robertojavadev.githubrepositoriesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositoriesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
